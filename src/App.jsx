c48b36258a7d30c4f0d6c85afe707823e4fe4ebf
iturres <emanueliturres.co@gmail.com>
2023-07-14 14:30:14 -0300

    fix:  properly enclose component return

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import Header from './components/Header';
 import BooksList from './components/containers/BooksList';
 import CategoriesList from './components/containers/CategoriesList';
 
-const App = () => {
+const App = () => (
   <>
     <Header />
     <Routes>
       <Route path="/" element={<BooksList />} />
       <Route path="/categories" element={<CategoriesList />} />
     </Routes>
-  </>;
-};
+  </>
+);
 
 export default App;
