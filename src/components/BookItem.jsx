aa8b0b2bd235b99c2c1cea22b4b3f66b049f6b2f
iturres <emanueliturres.co@gmail.com>
2023-07-14 14:32:09 -0300

    Add  -component-

---
+++ src/components/BookItem.jsx
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+import PercentageRing from './utils/PercentageRing';
+
+const BookItem = ({ book }) => (
+  <div className="book-item">
+    <div className="book-item__info">
+      <span>{book.category}</span>
+      <h2>{book.title}</h2>
+      <span>Author</span>
+      <div className="book-item__actions">
+        <button id={book.id} type="button">
+          Comments
+        </button>
+        <button id={book.id} type="button">
+          Remove
+        </button>
+        <button id={book.id} type="button">
+          Edit
+        </button>
+      </div>
+    </div>
+    <div className="book-item__percentage">
+      <PercentageRing percentage={book.percentage} />
+    </div>
+    <div className="book-item__chapter">
+      <span>CURRENT CHAPTER</span>
+      <h3>{book.chapter}</h3>
+      <button id={book.id} type="button">
+        UPDATE PROGRESS
+      </button>
+    </div>
+  </div>
+);
+
+BookItem.propTypes = {
+  book: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    chapter: PropTypes.string.isRequired,
+    percentage: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default BookItem;
