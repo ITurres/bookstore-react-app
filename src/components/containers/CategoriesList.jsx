50872fa425e5a6004274a9e8ec2475ee82f01f89
iturres <emanueliturres.co@gmail.com>
2023-07-14 14:34:30 -0300

    Add  -container-component-

---
+++ src/components/containers/CategoriesList.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+const CategoriesList = () => (
+  <div className="container categories">
+    <h2>
+      Categories page is being Build. Please go back to
+      <Link to="/">Books</Link>
+    </h2>
+  </div>
+);
+
+export default CategoriesList;
