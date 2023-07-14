e242f0de6f4d2ea444349f414e4ad7eec5cd8112
iturres <emanueliturres.co@gmail.com>
2023-07-14 16:04:53 -0300

    feat: Added new 'hardcoded examples' book items to  -container-component-

--- src/components/containers/BooksList.jsx
+++ src/components/containers/BooksList.jsx
@@ -5,9 +5,26 @@ const BooksList = () => {
     {
       id: 1,
       title: 'The Hunger Games',
+      author: 'Suzanne Collins',
       category: 'Action',
       chapter: 'Chapter 17',
-      percentage: '64',
+      percentage: 64,
+    },
+    {
+      id: 2,
+      title: 'Dune',
+      category: 'Science Fiction',
+      author: 'Frank Herbert',
+      chapter: 'Chapter 3: A Lesson Learned',
+      percentage: 8,
+    },
+    {
+      id: 3,
+      title: 'Capital in 20th Century',
+      author: 'Suzanne Collins',
+      category: 'Economy',
+      chapter: 'Introduction',
+      percentage: 0,
     },
   ];
 
