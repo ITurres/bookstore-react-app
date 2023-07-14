a5f31432ccaa9a29ea35e858c0038026dfe0b953
iturres <emanueliturres.co@gmail.com>
2023-07-14 14:23:00 -0300

    refactor: Renamed  import -file-path- on

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/index.scss';
-import App from './components/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
