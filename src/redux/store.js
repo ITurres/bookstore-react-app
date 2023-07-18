2ed67f71071571a5d45af1109db9e8ea49785cb1
iturres <emanueliturres.co@gmail.com>
2023-07-17 22:07:40 -0300

    feat: Import 'categoriesReducer' and add it to 'bookstore.reducer'

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/booksSlice';
+import categoriesReducer from './categories/categoriesSlice';
 
 const bookstore = configureStore({
   reducer: {
     books: booksReducer,
+    categories: categoriesReducer,
   },
 });
 
