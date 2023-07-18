18afd0c884a79f2a2cf15cf90dd9de28105c78fc
iturres <emanueliturres.co@gmail.com>
2023-07-18 19:55:49 -0300

    refactor: Set 'initialState' outside 'booksSlice' and fill it with hardcoded data

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,10 +1,37 @@
-import createSlice from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  books: [
+    {
+      item_id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+      totalPages: 200,
+      pagesRead: 50,
+    },
+    {
+      item_id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+      totalPages: 450,
+      pagesRead: 23,
+    },
+    {
+      item_id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+      totalPages: 500,
+      pagesRead: 145,
+    },
+  ],
+};
 
 const booksSlice = createSlice({
   name: 'books',
-  initialState: {
-    books: [],
-  },
+  initialState,
   reducers: {
     addBook: (state, action) => {
       const newBook = action.payload;
